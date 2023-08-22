bb9af063921800250f8ffc2537d297733065669c
Ramneekkaur8 <138462854+Ramneekkaur8@users.noreply.github.com>
2023-08-22 12:00:16 +0530

    Update module3.1.sol

--- module3.1.sol
+++ module3.1.sol
@@ -1,1 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
 
+contract MyToken{
+    // public variables here
+    string public tokenName="RAMNEEK";
+    string public tokenAbbrv="RAM";
+    uint public totalSupply=0;
+
+    // mapping variable here
+    mapping (address=>uint) public balance;
+
+     // mint function
+    function mint(address _address,uint value) public {
+        totalSupply += value;
+        balance[_address] += value;
+    }
+
+    // burn function
+    function burn(address _address,uint value) public {
+        require(balance[_address]>=value,"Value > balance");
+        totalSupply -= value;
+        balance[_address] -= value;
+        
+    }
+}
